9ab9103e24e9ae3688c5aab5f7a6f4c17397b9b7
mubashir-dev <mubashirmobi633@gmail.com>
2022-05-21 21:47:47 +0500

    brands api has been completed

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class BrandFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        return [
+            'title' => $this->faker->unique()->name(),
+            'description' => $this->faker->paragraph(),
+            'image' => 'https://france-export-fv-online.com/3484-home_default/sauce-heinz-tomato-ketchup-bio.jpg',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+ Route::prefix('v1')->group(function(){
+     //Brands
+     Route::get('/brnads',[BrandController::class,'index']);
+ });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
